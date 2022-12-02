b7308606c9223e5c0347f27b6804548d3361550f
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-12-02 14:17:16 +0300

    devtools active

---
+++ src/main/java/com/hkemal/utils/ConfigProgramming.java
@@ -0,0 +1,8 @@
+package com.hkemal.utils;
+
+public class ConfigProgramming {
+
+    //DevTools      => Live Reload
+    //File  => Settings => Compiler => Build Project Automatically(ok)
+    //File  => Advenced Settings => Allow-Auto make to start even(ok) => eski verisyonda yok
+}
