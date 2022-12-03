28707e78c61263d49a6f73858cbe2473917f3a72
hkemal <hasankemal96@gmail.com>
2022-12-03 17:35:58 +0300

    05-@Bean-initMethod-destroyMethod-@Scope

--- src/main/java/com/hkemal/KodluyoruzSpringBootApplication.java
+++ src/main/java/com/hkemal/KodluyoruzSpringBootApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 })
 public class KodluyoruzSpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KodluyoruzSpringBootApplication.class, args);
     }
--- src/main/java/com/hkemal/dto/BeanDTO.java
+++ src/main/java/com/hkemal/dto/BeanDTO.java
@@ -2,11 +2,12 @@ package com.hkemal.dto;
 
 import lombok.*;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-//@Log4j
+@Log4j
 @Builder
 @ToString
 public class BeanDTO {
@@ -16,4 +17,16 @@ public class BeanDTO {
     private String beanName;
 
     private String beanData;
+
+    //başlangıç
+    public void initialBeanMethod() {
+        log.info("Bean başlamadan önce ben varım");
+        System.out.println("Bean başlamadan önce çalışacak method");
+    }
+
+    //bitiş
+    public void destroyBeanMethod() {
+        log.info("Bean bittikten sonra ben varım");
+        System.err.println("Bean bittikten sonra çalışacak method");
+    }
 }
